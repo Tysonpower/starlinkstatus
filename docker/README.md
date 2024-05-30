02fc964f204b09ffbac44f3218a12755c7882d94
Tony Hallworth <tonyh@tgg-m1.local>
2024-05-30 22:13:00 +1000

    updated README for docker

--- docker/README.md
+++ docker/README.md
@@ -1,7 +1,24 @@
 # StarlinkStatus with docker
 
-Change YOURAPIKEY in docker-compose.yaml to api received from [https://starlinkstatus.space/](https://starlinkstatus.space/). Next run:
+Update docker-compose.yaml with the api received from [https://starlinkstatus.space/](https://starlinkstatus.space/).
 
-docker-compose up -d
+```yaml docker-compose.yaml
+version: '3.7'
+services:
+  starlinkstatus:
+    build:
+      context: .
+    restart: always
+    environment:
+      - APIKEY=YOURAPIKEY           # Replace YOURAPIKEY e.g APIKEY=a1b2c3d4e5f6
+      - SCHEDULE=900                # 900 secs = 15min, so when CRONJOB=false this will run every 15 min
+      - DISHY=false                 # Set to true to get Dish Stats
+      - SPEEDTEST=false             # Set to true to run Speedtests
+      - debug=true                  # Set to true to show debug data
+      - CRONJOB=false               # Set to true to run only once, especially as a scheduled job
+```
+Next run from your terminal
+
+`docker compose up -d`
 
 and you're ready :)
--- starlinkstatus_client.sh
+++ starlinkstatus_client.sh
@@ -4,6 +4,18 @@ VERSION=1.21
 APIURL="https://starlinkstatus.space/api/postresult"
 SERVERSURL="https://starlinkstatus.space/api/getservers"
 
+speedtest=false
+dishy=false
+apikey=""
+
+help() {
+    echo "Usage: starlinkstatus.sh -k APIKEY";
+    echo "-k | --key        API Key for starlinkstatus.space"
+    echo "-s | --speedtest  Enable Speedtest Data, requires speedtest.net cli"
+    echo "-d | --dishy      Enable Dishy Data, requires gprcurl"
+    echo "-h | --help       Show this Message"
+}
+
 pingservers() {
     cnt=$((${#SERVERS[@]} - 1))
 
@@ -29,88 +41,66 @@ getgeodata() {
     geojsn=$(curl -s http://ip-api.com/json/)
 }
 
+while [ "$1" != "" ]; do
+    case $1 in
+        -k | --key )            shift
+                                apikey="$1"
+                                ;;
+        -s | --speedtest )      speedtest=true
+                                ;;
+        -d | --dishy )          dishy=true
+                                ;;
+        -h | --help )           help
+                                exit
+                                ;;
+        * )                     help
+                                exit 1
+    esac
+    shift
+done
+
 # Main Script ----------------------------------------------------------------------------------
-main(){
-    echo "starlinkstatus.space Client - Version $VERSION"
-    if [ "$APIKEY" == "" ]
-    then
-        echo "APIKEY not set"
-        exit
-    fi
-    echo "API Key: $APIKEY"
+echo "starlinkstatus.space Client - Version $VERSION"
 
-    # get servers to ping
-    SERVERS=($(curl -s ${SERVERSURL}))
-    # Check if Servers to Ping are there, if not try again (could be caused by connection timeout)
-    if [ ${#SERVERS[@]} -gt 0 ]
-    then
-        SERVERS=($(curl -s ${SERVERSURL}))
-    fi
-
-    pingservers
-
-    # Check if Dishy Telemetry Enabled and run it
-    if [ $DISHY == true ]
-    then
-        grpcurl --version >/dev/null && echo "getting Dishy Data..." || { echo -e "\e[31mgrpcurl not found!\e[0m"; exit 1; }
-        dishstatus=$(grpcurl -plaintext -emit-defaults -d '{"getStatus":{}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle) || dishstatus="{}"
-        # dishcontext=$(grpcurl -plaintext -emit-defaults -d '{"dishGetContext":{}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle) || dishcontext="{}"
-        dishcontext="{}"    #this APi is sadly not useable anymore
-    else
-        dishstatus="{}"
-        dishcontext="{}"
-    fi
-
-    # Check if Speedtest Enabled and run it
-    if [ $SPEEDTEST == true ]
-    then
-        speedtest -V --accept-license --accept-gdpr >/dev/null && echo "speedtest is running..." || { echo -e "\e[31mSpeedtest CLI not found!\e[0m"; exit 1; }
-        st=$(speedtest --accept-license --accept-gdpr -f json)
-    else
-        st="{}"
-    fi
-
-    getgeodata
-
-    jsndata='{"key":"'$APIKEY'","geo":'$geojsn',"ping":'$pingjsn',"speed":'$st',"dishyStatus":'$dishstatus',"dishyContext":'$dishcontext',"version":'$VERSION'}'
-    
-    # Send data to API
-    curl -d "$jsndata" -H "Content-Type: application/json" -X POST $APIURL
-    echo ""
-    echo ""
-    echo ""
-    if [ $debug == true ]
-    then 
-        echo "jsndata"
-        echo "$jsndata"
-        echo 
-    fi
-}
-
-if [[ -z "$CRONJOB" ]] || [[ $CRONJOB == false ]]; then
-     
-    echo "Scheduled to run every: $SCHEDULE seconds"
-    while :
-    do
-        main
-        echo 
-        count=0 
-        until false 
-        do
-            ((count++))
-            if [[ $debug2 == true ]]
-            then 
-                echo "Counter = $count of $SCHEDULE"
-            fi
-
-            sleep 1
-            if [[ "$count" -ge "$SCHEDULE" ]] 
-            then 
-                break
-            fi
-        done
-    done
-else
-    echo "Running as CRONJOB"
-    main
+if [ "$apikey" == "" ]
+then
+    exit
 fi
+echo "API Key: $apikey"
+
+# get servers to ping
+SERVERS=($(curl -s ${SERVERSURL}))
+# Check if Servers to Ping are there, if not try again (could be caused by connection timeout)
+if [ ${#SERVERS[@]} -gt 0 ]
+then
+    SERVERS=($(curl -s ${SERVERSURL}))
+fi
+
+pingservers
+
+# Check if Dishy Telemetry Enabled and run it
+if [ $dishy == true ]
+then
+    grpcurl --version >/dev/null && echo "getting Dishy Data..." || { echo -e "\e[31mgrpcurl not found!\e[0m"; exit 1; }
+    dishstatus=$(grpcurl -plaintext -emit-defaults -d '{"getStatus":{}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle) || dishstatus="{}"
+    # dishcontext=$(grpcurl -plaintext -emit-defaults -d '{"dishGetContext":{}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle) || dishcontext="{}"
+    dishcontext="{}"    #this APi is sadly not useable anymore
+else
+    dishstatus="{}"
+    dishcontext="{}"
+fi
+
+# Check if Speedtest Enabled and run it
+if [ $speedtest == true ]
+then
+    speedtest -V --accept-license --accept-gdpr >/dev/null && echo "speedtest is running..." || { echo -e "\e[31mSpeedtest CLI not found!\e[0m"; exit 1; }
+    st=$(speedtest --accept-license --accept-gdpr -f json)
+else
+    st="{}"
+fi
+
+getgeodata
+jsndata='{"key":"'$apikey'","geo":'$geojsn',"ping":'$pingjsn',"speed":'$st',"dishyStatus":'$dishstatus',"dishyContext":'$dishcontext',"version":'$VERSION'}'
+# Send data to API
+curl -d "$jsndata" -H "Content-Type: application/json" -X POST $APIURL
+echo "\n"
